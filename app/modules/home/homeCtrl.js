439553a60069ae16898233e4d272f975d87c2fd7
Newtt <m.chatelan@gmail.com>
2018-01-10 10:52:55 +0100

    fini :)

--- app/modules/home/homeCtrl.js
+++ app/modules/home/homeCtrl.js
@@ -13,7 +13,7 @@
 		.module('state-manager')
 		.controller('HomeCtrl', Home);
 
-	Home.$inject = ['homeService'];
+	Home.$inject = ['homeService', '$interval'];
 
 	/*
 	* recommend
@@ -21,21 +21,20 @@
 	* and bindable members up top.
 	*/
 
-	function Home(homeService) {
+	function Home(homeService, $interval) {
 		/*jshint validthis: true */
 		var vm = this;
+		vm.updateDataFrequency = 5000;
+		vm.updateTimerFrequency = 1000;
 		vm.title = "State Manager";
 		vm.version = "1.0.0";
-		vm.statusAll = "All Systems Operational";
-		vm.lastUpdated = 23;
-		vm.daysSinceLastIncident = 0;
-		vm.activeMaintenances = 12;
-		vm.activeIncidents = 44;
+		vm.lastUpdated = 0;
+		vm.daysSinceLastIncident = "-";
 		vm.services = [
 			{
 				"self":"https://docs.angularjs.org/api/ng/directive/ngRepeat",
 				"name":"MySQL",
-				"state":"Operational",
+				"state":"up",
 				"statusAddress":"https://docs.angularjs.org/api/ng/directive/ngRepeat/234",
 				"description":"MySQL database",
 				"contact":"Matthieu Chatelan"
@@ -43,18 +42,42 @@
 			{
 				"self":"https://docs.angularjs.org/api/ng/directive/ngRepeat/234",
 				"name":"SSH",
-				"state":"Operational",
+				"state":"up",
 				"statusAddress":"https://docs.angularjs.org/api/ng/directive/ngRepeatasdsda",
 				"description":"Lol SSH ",
 				"contact":"Alain Hardy"
 			}
 		];
 
-		homeService.fetchData()
+		// Function used to get new data and update the page (automatically)
+		let updateData = () => {
+			homeService.fetchData()
 			.then(function(response) {
 				vm.greeting = response.data;
 				// vm.services = response.data;
+
+				vm.activeMaintenances = vm.services.filter((item) => {return item.state === "maintenance"}).length;
+				vm.downService = vm.services.filter((item) => {return item.state === "down"}).length;
+
+				vm.statusAll = !(vm.activeMaintenances || vm.downService);
+				vm.statusAll = vm.statusAll ? "all_good" : "all_error";
+
+				console.log("Data fetched");
+				vm.lastUpdated = 0;
 			});
+		};
+
+		// First call to display data before interval kicks in
+		updateData();
+
+		// Update the page with the latest info every "updateDataFrequency" ms
+		$interval(updateData, vm.updateDataFrequency);
+
+		// Update the page timer until reset when new data are fetched
+		$interval(() => {
+				vm.lastUpdated = vm.lastUpdated + (vm.updateTimerFrequency / 1000) < (vm.updateDataFrequency / 1000) ? vm.lastUpdated + (vm.updateTimerFrequency / 1000) : vm.lastUpdated = 0;
+				console.log(vm.lastUpdated);
+			}, vm.updateTimerFrequency);
 	}
 
 
