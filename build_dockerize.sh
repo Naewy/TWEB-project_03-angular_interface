894829b07c2392c4e387d7f235e78af1777ffab5
Newtt <m.chatelan@gmail.com>
2018-01-10 15:03:44 +0100

    fix script

--- build_dockerize.sh
+++ build_dockerize.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "Removing old angular folder if existant"
+rm -rf angular
+
 echo "Building with grunt..."
 
 grunt build >> /dev/null
@@ -13,6 +16,11 @@ then
 	mkdir angular
 	cp -r app/ angular
 	cp index.html angular
+
+	touch angular/Dockerfile
+	echo "FROM httpd:2.2" >> angular/Dockerfile
+	echo "MAINTAINER Matthieu Chatelan" >> angular/Dockerfile
+	echo "COPY data/ /usr/local/apache2/htdocs/" >> angular/Dockerfile
 
 	printf "\033[0;32mangular folder created\n\033[0m"
 else
