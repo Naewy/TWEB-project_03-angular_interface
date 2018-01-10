1127069dc0bc973646a5081452d2d92e8bcb57b6
Newtt <m.chatelan@gmail.com>
2018-01-10 14:54:08 +0100

    script done

--- build_dockerize.sh
+++ build_dockerize.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-echo "Totot"
+echo "Building with grunt..."
+
+grunt build >> /dev/null
+
+result=$?
+
+if [ $result -eq 0 ]
+then
+	printf "\033[0;32mBuild successful\n\033[0m"
+
+	mkdir angular
+	cp -r app/ angular
+	cp index.html angular
+
+	printf "\033[0;32mangular folder created\n\033[0m"
+else
+	printf "\033[0;31mBuild failed\n\033[0m"
+fi
+
+
+
